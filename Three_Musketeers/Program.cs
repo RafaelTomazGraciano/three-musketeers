de353d2f93a915b12157c1b8c35a01dbb305d718
HedroPedro <pedrohenrique.oliveira119@gmail.com>
2025-11-05 12:14:19 -0300

    feat: version

--- Three_Musketeers/Program.cs
+++ Three_Musketeers/Program.cs
@@ -4,40 +4,118 @@ using Three_Musketeers.Visitors;
 using Three_Musketeers.Listeners;
 using Three_Musketeers.Grammar;
 using System.CommandLine;
-using System.CommandLine.Parsing;
+using System.CommandLine.Invocation;
 
-namespace Three_Musketeers {
+namespace Three_Musketeers 
+{
     public class Program
     {
+        public static readonly string VERSION = "v0.9.5";
+        private static readonly CliArgument<string> input = new("input") {
+            Description = "Path of the source file to compile",
+            Arity = ArgumentArity.ZeroOrOne
+        };
 
-        private static readonly Argument<string> input = new("input")
-            {
-                Description = "File to be compiled",
-            };
-
-        private static readonly Option<string> output = new("-o", ["--out"])
+        private static readonly CliOption<string> output = new("-o", ["--out"])
         {
-            Description = "Path of executable",
-            DefaultValueFactory = value => "a.out"
+            DefaultValueFactory = (res) => "a.out",
+        };
+
+        private static readonly CliOption<uint> compilerOptimazionLevel = new("-O",
+            aliases: ["--opt"])
+        {
+            DefaultValueFactory = (res) => 2,
+            Description = "Optimization level (0-3)",
+        };
+
+        private static readonly CliOption<string> dotLLCodePath = new("--ll",
+        )
+        {
+            DefaultValueFactory = (res) => "",
+            Description = "Path of generated LLVM IR code"
+        };
+
+        private static readonly CliOption<bool> addDebugFlagToGcc = new("-g")
+        {
+            DefaultValueFactory = (res) => false,
+            Description = "Add debug information to the generated code"
+        };
+
+        private static readonly CliOption<string> includeLibrary = new("-I", [ "--Include"])
+        {
+            DefaultValueFactory = (res) => "",
+            Description = "Include path libraries"
         };
 
         public static int Main(string[] args)
         {
-            RootCommand rootCommand = new("Three Musketeers Language Compiler");
-            rootCommand.Add(input);
-            rootCommand.Add(output);
-
-            var result = rootCommand.Parse(args);
-            if (result.Errors.Count > 0)
+            var rootCommand = new CliRootCommand("Three Musketeers Language Compiler")
             {
-                foreach (ParseError parseError in result.Errors)
+                input,
+                output,
+                compilerOptimazionLevel,
+                dotLLCodePath,
+                addDebugFlagToGcc,
+                includeLibrary
+            }!;
+
+            foreach (var option in rootCommand.Options)
+            {
+                if (option.Name.Contains("version"))
                 {
-                    Console.Error.WriteLine(parseError.Message);
+
+                    option.Action = new VersionAction();
+                    option.Aliases.Add("-v");
+                }
+            }
+
+            rootCommand.SetAction((result) =>
+            {
+                string? inputPath = result.GetValue(input);
+                string outputPath = result.GetValue(output)!;
+                uint optLevel = result.GetValue(compilerOptimazionLevel);
+                string llPath = result.GetValue(dotLLCodePath)!;
+                bool debugFlag = result.GetValue(addDebugFlagToGcc)!;
+                string includeLib = result.GetValue(includeLibrary)!;
+
+                return CompileFile(inputPath, outputPath, optLevel, llPath, debugFlag, includeLib);
+            });
+
+            CliConfiguration config = new(rootCommand);
+
+            var parserResult = config.Parse(args);
+            if (parserResult.Errors.Count > 0)
+            {
+                foreach (var error in parserResult.Errors)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(error.Message);
+                    Console.ResetColor();
                 }
                 return 1;
             }
-            string filePath = result.GetValue(input)!;
-            string resultPath = result.GetValue(output)!;
+
+            return parserResult.Invoke();
+        }
+
+        private static int CompileFile(string? filePath, string resultPath,
+                                       uint optimizationLevel, string llCodePath,
+                                       bool addDebugFlag, string includeLib)
+        {
+            if (filePath == null)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("No input file specified.");
+                Console.ResetColor();
+                return 1;
+            }
+            
+            bool dontSaveDotLL = string.IsNullOrEmpty(llCodePath);
+            if (dontSaveDotLL)
+            {
+
+                llCodePath = Path.GetTempFileName();
+            }
 
             try
             {
@@ -45,13 +123,16 @@ namespace Three_Musketeers {
                 var inputStream = new AntlrFileStream(filePath);
                 var lexer = new ExprLexer(inputStream);
                 var tokenStream = new CommonTokenStream(lexer);
+
                 // Syntax Analysis
                 var parser = new ExprParser(tokenStream);
-                //Listeners
+
+                // Listeners
                 parser.RemoveErrorListeners();
                 var errorListener = new CompilerErrorListener();
                 parser.AddErrorListener(errorListener);
-                //AST
+
+                // AST
                 var tree = parser.start();
 
                 if (errorListener.hasErrors)
@@ -62,8 +143,8 @@ namespace Three_Musketeers {
                     return 1;
                 }
 
-                //Semantic Analysis
-                var semanticAnalyzer = new SemanticAnalyzer();
+                // Semantic Analysis
+                var semanticAnalyzer = new SemanticAnalyzer(includeLib);
                 semanticAnalyzer.Visit(tree);
 
                 if (semanticAnalyzer.hasErrors)
@@ -77,17 +158,20 @@ namespace Three_Musketeers {
                 // Intermediate Code generation
                 var codeGenerator = new CodeGenerator();
                 var llvmCode = codeGenerator.Visit(tree);
-                string outputPath = Path.ChangeExtension(filePath, ".ll");
-                string bytecodePath = Path.ChangeExtension(filePath, ".bc");
-                string optBytecodePath = bytecodePath.Replace(".bc", "-opt.bc");
-                string assemblyPath = Path.ChangeExtension(filePath, ".s");
+                string outputPath = Path.ChangeExtension(llCodePath, ".ll");
+                string assemblyPath = Path.ChangeExtension(outputPath, ".s");
 
                 File.WriteAllText(outputPath, llvmCode);
-                Process.Start("llvm-as", $"{outputPath} -o {bytecodePath}").WaitForExit();
-                Process.Start("opt", $"-O2 {bytecodePath} -o {optBytecodePath}").WaitForExit();
-                Process.Start("llc", $"{optBytecodePath} -o {assemblyPath}").WaitForExit();
-                Process.Start("gcc", $"{assemblyPath} -o {resultPath} -no-pie").WaitForExit();
-                Process.Start("rm", $"{bytecodePath} {optBytecodePath} {assemblyPath}");
+
+                Process.Start("llc", $"{outputPath} -O{optimizationLevel} -o {assemblyPath}")?.WaitForExit();
+                Process.Start("gcc", $"{(addDebugFlag ? "-g" : "")} {assemblyPath} -o {resultPath} -no-pie")?.WaitForExit();
+                File.Delete(assemblyPath);
+
+                if (dontSaveDotLL)
+                {
+                    File.Delete(outputPath);
+                }
+
                 return 0;
             }
             catch (Exception ex)
@@ -99,6 +183,15 @@ namespace Three_Musketeers {
                 Console.ResetColor();
                 return 1;
             }
+        }
+    }
+    
+    public sealed class VersionAction : SynchronousCliAction
+    {
+        public override int Invoke(ParseResult parseResult)
+        {
+            parseResult.Configuration.Output.WriteLine($"Three Musketeers Compiler {Program.VERSION}");
+            return 0;
         }
     }
 }
