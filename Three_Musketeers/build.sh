09f649011cb99d3176a651391953e9204083e577
Pedro Henrique de Oliveira <pedrohenrique.oliveira119@gmail.com>
2025-11-18 18:19:19 -0300

    feat: add --bin option and logic

--- Three_Musketeers/build.sh
+++ Three_Musketeers/build.sh
@@ -7,7 +7,7 @@ dotnet build --nologo -v q || exit 1
 dotnet run --no-build -- Examples/code.3m -o code || exit 1
 
 # Execute
-./Examples/bin/code arg1 arg2 arg3
+./Examples/code arg1 arg2 arg3
 
 EXIT_CODE=$?
 echo "===================================="
